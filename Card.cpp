a3c2665ce0bbd4c82d3fc8dda162e27886265cb7
Dashix111 <bowend@umich.edu>
2018-12-21 13:54:45 -0600

    pack.h

--- Card.cpp
+++ Card.cpp
@@ -1,8 +1,9 @@
 //
-//  main.cpp
-//  doudizhu
-//
-//  Created by Bowen Dong on 12/21/18.
-//  Copyright © 2018 Bowen Dong. All rights reserved.
-//
+//  Card.cpp
+#include <string>
+#include <iostream>
+
+#include "Card.h"
+
+
 
